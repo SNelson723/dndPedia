aab0036f1800197884c588499f7f68996137dfdf
SNelson723 <stephenn649@gmail.com>
2024-12-27 17:03:44 -0600

    Fixed the NabBar route links

--- src/NavBar/NavBar.jsx
+++ src/NavBar/NavBar.jsx
@@ -1,10 +1,20 @@
-const NavBar = () => {
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { Link } from 'react-router-dom';
 
+const NavBar = () => {
   return (
-    <>
-      NavBar
-    </>
+    <Navbar bg="dark" data-bs-theme="dark">
+      <Container>
+        <Navbar.Brand as={Link} to="/">Navbar</Navbar.Brand>
+        <Nav className="me-auto">
+          <Nav.Link as={Link} to="/general">General</Nav.Link>
+          {/* Add more Nav.Link components as needed */}
+        </Nav>
+      </Container>
+    </Navbar>
   )
 };
 
-export default NavBar
+export default NavBar;
