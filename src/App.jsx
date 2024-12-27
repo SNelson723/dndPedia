110d3ad6b2e1b6c4d5cf8240e63b81b168a370a3
SNelson723 <stephenn649@gmail.com>
2024-12-27 16:43:34 -0600

    Setup the initial project, making the game plan now, just going to be a front end project

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+// import { useState } from 'react';
+import './App.css';
+
+/**
+ * 
+ * TODO:
+ * 
+ *  Need a FormSelector component to determine the API link
+ *  Need to create a container for cards so I can click on all the options
+ *  Need a space for clicking Submit or Cancel the query
+ *  Need a way to pop up a modal that will display more information or conditionally render the additional data
+ */
+
+function App() {
+
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default App
