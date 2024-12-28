e02357601c989eeabc6bd36b7c2d6c2d5c2e3fba
SNelson723 <stephenn649@gmail.com>
2024-12-27 18:15:26 -0600

    Added all the images for the classes in ClassSearch. Now I need to finish styling the cards with the names and get them more proportional

--- src/SearchComponents/ClassSearch.jsx
+++ src/SearchComponents/ClassSearch.jsx
@@ -1,15 +1,29 @@
-import { useState, useEffect } from 'react';
+/* eslint-disable react/prop-types */
+import { useState } from 'react';
 import { Container, Row, Col, Card, Image, Button } from 'react-bootstrap';
 
 const ClassSearch = ({ classes }) => {
   const [selectedClass, setSelectedClass] = useState('');
 
+  console.log('Class Search', classes)
   return (
-    <div className="background ">
-    {classes.map((class, i) => (
-
-    ))}
-  </div>
+    <>
+      <Container>
+        <Row  xs={1} md={2} lg={3} className="g-4">
+          {classes.map((classObj, i) => (
+            <Col key={i}>
+              <Card className='card card w-75 mx-auto'>
+                <Image
+                  src={`../src/resources/${classObj.name}.jpg`}
+                  alt={classObj.name}
+                  fliud
+                />
+              </Card>
+            </Col>
+          ))}
+        </Row>
+      </Container>
+    </>
   )
 };
 
