1bb543ccdd63e14cb0ff81484589a06902589d2b
SNelson723 <stephenn649@gmail.com>
2024-12-29 17:28:09 -0600

    Working on expanding the General/Overview tab for the classes in a way Class Tab can be reused for all classes

--- src/SearchComponents/ClassTabs/ClassTab.jsx
+++ src/SearchComponents/ClassTabs/ClassTab.jsx
@@ -1,3 +1,5 @@
+import { Row, Col } from 'react-bootstrap';
+
 const skills = {
   barbarian: 'Strength',
   bard: 'Charisma',
@@ -7,18 +9,31 @@ const skills = {
   monk: 'Dexterity & Wisdom',
   paladin: 'Strength & Wisdom',
   ranger: 'Dexterity & Wisdom',
-  
+  roque: 'Dexterity',
+  sorcerer: 'Charisma',
+  warlock: 'Charisma',
+  wizard: 'Intelligence'
 };
 
 const ClassTab = ({ classInfo, type }) => {
 
   // console.log(classInfo, type);
   const primaryAbility = skills[classInfo.index];
-  console.log(primaryAbility);
+  // console.log(primaryAbility);
 
   return (
     <div>
-      {type === 'general' ? <div>General</div> : null}
+      {type === 'general' ?
+        <>
+          <Row className='m-3'>
+            <Col xs='1' md='1' lg='2' style={{fontWeight: 'bold'}}>Primary Abilities: </Col>
+            <Col xs='1' md='2' lg='3'>{primaryAbility}</Col>
+          </Row>
+          <Row>
+            <Col>Hit Point Die: </Col>
+          </Row>
+        </>
+      : null}
       {type === 'proficiency' ? <div>Proficiencies</div> : null}
     </div>
   );
